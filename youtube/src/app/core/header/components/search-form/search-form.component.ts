89bf45ea60687b6c9f978947fae86345f0abc241
Jiri <jirisimonov@gmail.com>
2023-03-06 10:39:51 +0300

    feat: add search form

--- youtube/src/app/core/header/components/search-form/search-form.component.ts
+++ youtube/src/app/core/header/components/search-form/search-form.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
   styleUrls: ['./search-form.component.scss'],
 })
-export class SearchFormComponent {}
+export class SearchFormComponent implements OnInit {
+  public searchForm!: FormGroup;
+  ngOnInit(): void {
+    this.searchForm = new FormGroup({
+      search: new FormControl('', [Validators.required]),
+    });
+    this.searchForm.valueChanges.pipe(throttleTime(800)).subscribe();
+  }
+  onSubmit() {
+    console.log(this.searchForm.value);
+  }
+}
--- youtube/src/app/core/header/header.module.ts
+++ youtube/src/app/core/header/header.module.ts
@@ -6,10 +6,11 @@ import { FilterComponent } from './components/filter/filter.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { UserComponent } from './components/user/user.component';
 import { MaterialModule } from '../material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HeaderPageComponent, SearchFormComponent, FilterComponent, SettingsComponent, UserComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, ReactiveFormsModule],
   exports: [HeaderPageComponent],
 })
 export class HeaderModule {}
