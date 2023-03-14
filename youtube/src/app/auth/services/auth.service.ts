31114fb573c67ac74d644db1fad7b1f397375730
Jiri <jirisimonov@gmail.com>
2023-03-14 17:51:29 +0300

    feat: add storage module

--- youtube/src/app/auth/services/auth.service.ts
+++ youtube/src/app/auth/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
+import { LocalStorageService } from 'src/app/core/storage/services/local-storage.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,16 +10,16 @@ export class AuthService {
   private isUser$$ = new BehaviorSubject<string>('Your Name');
   public isUser$ = this.isUser$$.asObservable();
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private localStorage: LocalStorageService) {}
 
   public signIn(username: string): void {
     this.isUser$$.next(username);
-    localStorage.setItem('auth', username);
+    this.localStorage.setItem('auth', username);
     this.router.navigate(['youtube'], { replaceUrl: true }).catch();
   }
 
   public signOut(): void {
     this.isUser$$.next('Your Name');
-    localStorage.clear();
+    this.localStorage.clear();
   }
 }
