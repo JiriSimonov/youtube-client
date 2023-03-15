d8a1d1d7a60cdc506458bad7f06373af59431f13
Jiri <jirisimonov@gmail.com>
2023-03-15 17:22:25 +0300

    refactor: video card

--- youtube/src/app/app.module.ts
+++ youtube/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { NgOptimizedImage } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, NoopAnimationsModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, NoopAnimationsModule, HttpClientModule, NgOptimizedImage],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
