efa7aa765c02554b7563989b3a3661204402881c
Jiri <jirisimonov@gmail.com>
2023-03-19 20:52:31 +0300

    feat: add validation for create card form

--- youtube/src/app/admin/components/create-card/create-card.component.ts
+++ youtube/src/app/admin/components/create-card/create-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { urlPattern } from 'src/app/shared/validators/validators';
 
 @Component({
   selector: 'app-create-card',
@@ -18,8 +19,24 @@ export class CreateCardComponent implements OnInit {
     this.createForm = new FormGroup({
       title: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
       description: new FormControl('', [Validators.maxLength(255)]),
-      img: new FormControl('', [Validators.required]),
-      link: new FormControl('', [Validators.required]),
+      img: new FormControl('', [Validators.required, Validators.pattern(urlPattern)]),
+      link: new FormControl('', [Validators.required, Validators.pattern(urlPattern)]),
     });
+  }
+
+  public get titleControl(): FormControl<string | null> {
+    return this.createForm.controls.title;
+  }
+
+  public get descriptionControl(): FormControl<string | null> {
+    return this.createForm.controls.description;
+  }
+
+  public get imgControl(): FormControl<string | null> {
+    return this.createForm.controls.img;
+  }
+
+  public get linkControl(): FormControl<string | null> {
+    return this.createForm.controls.link;
   }
 }
