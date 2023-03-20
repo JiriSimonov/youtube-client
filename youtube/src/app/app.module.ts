7c287d87a61b4b6ef1d7c49ee6b3da804bf8f7b5
Jiri <jirisimonov@gmail.com>
2023-03-20 19:19:02 +0300

    feat: add ngrx dependencies

--- youtube/src/app/app.module.ts
+++ youtube/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -7,10 +7,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { YoutubeInterceptor } from './youtube/interceptors/youtube.interceptor';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, NoopAnimationsModule, HttpClientModule, NgOptimizedImage],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, NoopAnimationsModule, HttpClientModule, NgOptimizedImage, StoreModule.forRoot(reducers, {
+      metaReducers
+    }), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }), EffectsModule.forRoot([])],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: YoutubeInterceptor, multi: true }],
   bootstrap: [AppComponent],
 })
